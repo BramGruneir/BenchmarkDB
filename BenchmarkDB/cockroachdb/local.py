fecd2c39ad57b798aa4886ccc40d227a706b631b
Bram Gruneir <bram@cockroachlabs.com>
2019-04-30 16:27:51 -0400

    Get this working with updated dependencies and Cockroach.

---
+++ BenchmarkDB/cockroachdb/local.py
@@ -0,0 +1,14 @@
+"""
+All settings for the postgreSQL module
+"""
+
+POSTGRESQL_NODES = {
+    'POSTGRESQL_1': 'localhost',
+}
+
+POSTGRESQL_PORT = 26257
+
+POSTGRESQL_USER = 'root'
+POSTGRESQL_PASSWORD = ''
+
+NUMBER_OF_NODES = 1
